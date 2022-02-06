ea220d6ca3b1757decbc77de9ef89b6b1953406f
gylim0604 <gylim0604@hotmail.com>
2022-02-06 20:52:21 +0800

    added eslint

--- index.js
+++ index.js
@@ -9,101 +9,102 @@ app.use(express.json());
 app.use(express.static('build'));
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message);
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message });
-    }
-    next(error);
+	console.log(error.message);
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' });
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message });
+	}
+	next(error);
 };
 
 app.use(errorHandler);
 
 let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true,
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only Javascript',
-        date: '2019-05-30T18:39:34.091Z',
-        important: false,
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        important: true,
-    },
+	{
+		id: 1,
+		content: 'HTML is easy',
+		date: '2019-05-30T17:30:31.098Z',
+		important: true,
+	},
+	{
+		id: 2,
+		content: 'Browser can execute only Javascript',
+		date: '2019-05-30T18:39:34.091Z',
+		important: false,
+	},
+	{
+		id: 3,
+		content: 'GET and POST are the most important methods of HTTP protocol',
+		date: '2019-05-30T19:20:14.298Z',
+		important: true,
+	},
 ];
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World! </h1>');
+	response.send('<h1>Hello World! </h1>');
 });
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then((notes) => {
-        response.json(notes);
-    });
+	Note.find({}).then((notes) => {
+		response.json(notes);
+	});
 });
 
 app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then((note) => {
-            if (note) {
-                response.json(note);
-            } else {
-                response.status(404).end();
-            }
-        })
-        .catch((error) => next(error));
+	Note.findById(request.params.id)
+		.then((note) => {
+			if (note) {
+				response.json(note);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then((result) => {
-            response.status(204).end();
-        })
-        .catch((error) => next(error));
+app.delete('/api/notes/:id', (request, response, next) => {
+	Note.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
 const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-    return maxId + 1;
+	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+	return maxId + 1;
 };
 
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body;
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    });
-    note.save()
-        .then((savedNote) => {
-            response.json(savedNote.toJSON());
-        })
-        .catch((error) => next(error));
+	const body = request.body;
+	const note = new Note({
+		content: body.content,
+		important: body.important || false,
+		date: new Date(),
+		id: generateId(),
+	});
+	note.save()
+		.then((savedNote) => {
+			response.json(savedNote.toJSON());
+		})
+		.catch((error) => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body;
-    const note = {
-        content: body.content,
-        important: body.important,
-    };
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-        .then((updatedNote) => {
-            response.json(updatedNote);
-        })
-        .catch((error) => next(error));
+	const body = request.body;
+	const note = {
+		content: body.content,
+		important: body.important,
+	};
+	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+		.then((updatedNote) => {
+			response.json(updatedNote);
+		})
+		.catch((error) => next(error));
 });
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port${PORT}`);
--- models/note.js
+++ models/note.js
@@ -1,37 +1,38 @@
 const mongoose = require('mongoose');
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI;
 
 console.log('connectiong to', url);
 
 mongoose
-    .connect(url)
-    .then((result) => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
-    });
+	.connect(url)
+	.then(() => {
+		console.log('connected to MongoDB');
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message);
+	});
 
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLenght: 5,
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-    },
-    important: Boolean,
+	content: {
+		type: String,
+		minLenght: 5,
+		required: true,
+	},
+	date: {
+		type: Date,
+		required: true,
+	},
+	important: Boolean,
 });
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    },
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
 });
 
 module.exports = mongoose.model('Note', noteSchema);
